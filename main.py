c0711dd948d3aefe371ee6ab193090b5c629def1
Marcos Vinicius <marcos2005.machado@gmail.com>
2025-06-10 11:01:33 -0300

    Adicionamos arquivo de execução principal main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from sys import argv
+from analisador_lexico import AnalisadorLexico
+
+def main():
+    if len(argv) != 3:
+        print("Uso: python main.py <entrada.txt> <saida.txt>")
+        return
+    
+    if not argv[1].endswith('.txt') or not argv[2].endswith('.txt'):
+        print("Os arquivos de entrada e saída devem ter a extensão .txt")
+        return
+    
+    entrada = argv[1]
+    saida = argv[2]
+
+    analisador = AnalisadorLexico(entrada, saida)
+    analisador.analisar()
+
+if __name__ == "__main__":
+    main()
